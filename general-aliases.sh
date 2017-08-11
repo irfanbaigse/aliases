531b1c46619323f62fc24d0022a2dc1876333529
irfanbaigdev <irfan.blackhawk@gmail.com>
2017-08-12 01:49:57 +0400

    general aliases updated

--- general-aliases.sh
+++ general-aliases.sh
@@ -1,7 +1,7 @@
 #General Aliases
 
-alias myip="curl http://ipecho.net/plain"
-alias hisg="history | grep" 
+alias myip="curl http://ipecho.net/plain" #display ip address
+alias hisg="history | grep" #searches you command history
 alias ps-grep="ps aux | grep" # e.g."ps-grep java" will list processes that have java in description
 alias ping="ping -o" #pings and stops
 alias rm="rm -i" #moves to trash
