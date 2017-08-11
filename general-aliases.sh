5ec9bb41d53bad98f49166bb060843e66ac016bd
irfanbaigdev <irfan.blackhawk@gmail.com>
2017-08-12 01:47:48 +0400

    general aliases

---
+++ general-aliases.sh
@@ -0,0 +1,9 @@
+#General Aliases
+
+alias myip="curl http://ipecho.net/plain"
+alias hisg="history | grep"
+alias ps-grep="ps aux | grep" # e.g."ps-grep java" will list processes that have java in description
+alias ping="ping -o"
+alias rm="rm -i"
+# This forces a shutdown.
+alias poweroff='sudo /sbin/shutdown -h now'
