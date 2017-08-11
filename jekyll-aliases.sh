18de5fa59cd67b725c6929926944619d9ebd3885
irfanbaigdev <irfan.blackhawk@gmail.com>
2017-08-12 01:43:59 +0400

    jekyll aliases

---
+++ jekyll-aliases.sh
@@ -0,0 +1,14 @@
+#jekyll
+
+alias j='jekyll'
+alias j-n='jekyll new'
+alias j-b='jekyll build'
+alias j-s='jekyll serve'
+alias j-s-dev="bundle exec jekyll serve --config _config.yml,_config.dev.yml"
+
+#Jekyll with Bundler
+alias jb-c='bundle exec jekyll clean'
+alias jb-b='bundle exec jekyll build' 
+alias jb-bw='bundle exec jekyll build --watch' 
+alias jb-s='bundle exec jekyll serve'
+alias jb-s-dev='bundle exec jekyll build --config _config.yml,_config_dev.yml'
