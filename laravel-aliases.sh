54fd0548ac28aabcffb399a64cdde4ee73762b06
irfanbaigdev <irfan.blackhawk@gmail.com>
2017-08-14 01:18:27 +0400

    laravel-aliases

---
+++ laravel-aliases.sh
@@ -0,0 +1,16 @@
+#Laravel
+
+alias art='php artisan'
+alias amr='php artisan migrate:refresh'
+alias ams='php artisan migrate --seed'
+alias amrs='php artisan migrate:refresh --seed'
+
+alias ci='composer install'
+alias cu='composer update'
+alias cdo='composer dump-autoload -o'
+
+# homestead
+alias vu='vagrant up'
+alias hs='homestead'
+alias hsp='homestead provision'
+alias hsu='homestead up'
